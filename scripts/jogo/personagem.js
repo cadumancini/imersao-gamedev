e3eb3409129f5c3ec0854c451285499d41e9ab6b
cadumancini <cadumancini@gmail.com>
2020-06-24 09:17:15 -0300

    Primeiro commit (aula 2 em andamento)

---
+++ scripts/jogo/personagem.js
@@ -0,0 +1,7 @@
+class Personagem extends Animacao{
+  constructor (matriz, imagem, x, largura, altura, 
+    larguraSprite, alturaSprite) {
+    super (matriz, imagem, x, largura, altura, 
+      larguraSprite, alturaSprite);
+  }
+}
