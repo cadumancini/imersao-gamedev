d0421e4ec99f33810e4a4323437dd7e4308aca08
cadumancini <cadumancini@gmail.com>
2020-07-06 12:22:18 -0300

    Acrescentando delay ao inimigo, para não aparecer de volta na tela logo após sumir.

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,15 +1,17 @@
 class Inimigo extends Animacao {
   constructor (matriz, imagem, x, variacaoY, largura, altura, 
-    larguraSprite, alturaSprite, velocidade) {
+    larguraSprite, alturaSprite, velocidade, delay) {
     super (matriz, imagem, x, variacaoY, largura, altura, 
       larguraSprite, alturaSprite);
     this.velocidade = velocidade;
+    this.delay = delay;
+    this.x = width + delay;
   }
 
   move () {
     this.x -= this.velocidade;
 
-    if(this.x < -this.largura)
+    if(this.x < -this.largura - this.delay)
       this.x = width;
   }
 }
